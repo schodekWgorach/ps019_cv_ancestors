0cf9ac2589729795ae293bb28a633682e1adeb09
schodekWgorach <sschodnicki@gmail.com>
2025-01-24 11:42:01 +0100

    create .gitignore

--- assets/data.js
+++ assets/data.js
@@ -1,4 +1,4 @@
-/*const cvancestors = [
+export const cvancestors = [
     {
         name: 'Maria Rój',
         photoUrl: "assets/images/ry_roj_maria.jpg",
@@ -12,7 +12,6 @@
             malesuada non, venenatis sit amet mi. Praesent sagittis volutpat arcu, vitae fringilla erat.
              Quisque nec lectus id erat porttitor
          dignissim ut vel magna. Vestibulum gravida convallis urna, vitae rutrum enim iaculis at.`
-
     },
     {
         name: 'Schodnicka Bogumiła',
@@ -27,7 +26,6 @@
             malesuada non, venenatis sit amet mi. Praesent sagittis volutpat arcu, vitae fringilla erat.
              Quisque nec lectus id erat porttitor
          dignissim ut vel magna. Vestibulum gravida convallis urna, vitae rutrum enim iaculis at. `
-
     },
     {
         name: 'Schodnicka Stanisława',
@@ -35,30 +33,5 @@
         text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In malesuada felis eu
          ultrices dignissim. Nam facilisis justo dolor, eget cursus mi tincidunt pellentesque. 
          Ut sit amet sollicitudin nunc. Aenean turpis ligula, pulvinar nec metus sagittis, vestibulum`
-
     }
-];*/
-
-import { cvancestors } from './assets/data.js';
-
-const imageElement = document.querySelector('.ancestores__foto');
-const textElement = document.querySelector('.ancestores__text');
-const nameElement = document.querySelector('.ancestores__username');
-
-let idx = 0;
-
-function updatecv()  {
-    const { name, photoUrl, text } = cvancestors[idx];
-    console.log(`Updating: ${name}`);
-    nameElement.innerText = name;
-    imageElement.src = photoUrl;
-    textElement.innerText = text;
-   
-    idx = (idx + 1) % cvancestors.length;
-    
-
-    setTimeout(updatecv, 2000);
-}
-
-updatecv(); // Initial call to start the animation. 2 seconds after page load. 2000 milliseconds = 2 seconds.
-
+];
